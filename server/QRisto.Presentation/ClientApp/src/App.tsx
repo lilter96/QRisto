b7a0b27bb112775b55322e42c5bfecb04b7b47fe
Terentiy Gatsukov <lilter96@mail.ru>
2023-12-13 14:47:33 +0300

    Add first migration, fix client issue

--- server/QRisto.Presentation/ClientApp/src/App.tsx
+++ server/QRisto.Presentation/ClientApp/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Login/>}></Route>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path="/restaurants"/>
