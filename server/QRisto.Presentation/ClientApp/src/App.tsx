2f79ce2dfd9eebad1c07a0bb824b5bed56b5effa
Terentiy Gatsukov <lilter96@mail.ru>
2023-12-15 01:00:51 +0300

    Lol

--- server/QRisto.Presentation/ClientApp/src/App.tsx
+++ server/QRisto.Presentation/ClientApp/src/App.tsx
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import AppRoutes from './routes/routes';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login/>}></Route>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path="/restaurants"/>
-          <Route path="/booking"/>
-        </Routes>
-        <footer></footer>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login/>}></Route>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/register' element={<Register/>}/>
+                    <Route path="/restaurants" element={<Register/>}/>
+                    <Route path="/booking"/>
+                </Routes>
+                <footer></footer>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
